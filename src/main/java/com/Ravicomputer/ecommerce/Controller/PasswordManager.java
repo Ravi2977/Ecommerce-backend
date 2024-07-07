e5622f57279d3dc7d1b0ac2dba8ecc1745a2b1a9
ravi2977 <mauryaravi599@gmail.com>
2024-07-07 14:13:37 +0530

    PAssword update method added to password manager controller cross

--- src/main/java/com/Ravicomputer/ecommerce/Controller/PasswordManager.java
+++ src/main/java/com/Ravicomputer/ecommerce/Controller/PasswordManager.java
@@ -4,14 +4,12 @@ import com.Ravicomputer.ecommerce.Model.UserModel;
 import com.Ravicomputer.ecommerce.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "https://ecommerce-backend-bmf8.onrender.com", "https://ravi-computer-ecommerce.vercel.app"})
 @RequestMapping("/password")
 public class PasswordManager {
     @Autowired
